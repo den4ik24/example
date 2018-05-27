688761ab33726adefe79cbe21fac00605087c96a
Yurchenko Denis <den4ik24@gmail.com>
2018-05-27 14:49:47 +0300

    27.05

--- example/Program.cs
+++ example/Program.cs
@@ -16,34 +16,59 @@ namespace example
             {
                 Console.WriteLine("\n(X + Y * Z)/P");
 
-               
+                try
+                {
 
-                int X;
-                Console.WriteLine("первое число равно");
-                X = Convert.ToInt32(Console.ReadLine());
+                    int X;
+                    Console.WriteLine("первое число равно");
+                    X = Convert.ToInt32(Console.ReadLine());
 
-                int Y;
-                Console.WriteLine("второе число равно");
-                Y = Convert.ToInt32(Console.ReadLine());
+                    int Y;
+                    Console.WriteLine("второе число равно");
+                    Y = Convert.ToInt32(Console.ReadLine());
 
-                int Z;
-                Console.WriteLine("третье число равно");
-                Z = Convert.ToInt32(Console.ReadLine());
+                    int Z;
+                    Console.WriteLine("третье число равно");
+                    Z = Convert.ToInt32(Console.ReadLine());
 
-                double P;
-                Console.WriteLine("четвертое число равно");
-                P = Convert.ToInt32(Console.ReadLine());
+                    double P;
+                    Console.WriteLine("четвертое число равно");
+                    P = Convert.ToInt32(Console.ReadLine());
+                    if (P == 0)
+                    {
+                        Console.WriteLine("на ноль делить нельзя");
+                        
 
-                double
-                    E = (X + Y * Z) / P;
+                    }
+
+                    double
+                        E = (X + Y * Z) / P;
 
 
-                Console.WriteLine("\n({0} + {1} * {2})/{3} = ", X, Y, Z, P);
-                Console.WriteLine("Результат : {0} \n", E);
+                    Console.WriteLine("\n({0} + {1} * {2})/{3} = ", X, Y, Z, P);
+                    Console.WriteLine("Результат : {0} \n", E);
 
-                string ext = Console.ReadLine();
-                if (ext == "exit")
-                    break;
+
+                    Console.WriteLine("EXIT Y/N ?");
+                    string ext = Console.ReadLine();
+                    if (ext == "Y")
+
+                        break;
+
+                    if (ext == "y")
+
+                        break;
+
+                   
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Произошло деление на ноль");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Вы ввели не число");
+                }
 
                 Console.ReadKey();
 
